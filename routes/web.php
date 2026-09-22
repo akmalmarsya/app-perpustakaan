1643c59ed59bfa09a1fab176340318bcd494e339
Akmal Marsya <akmalrasya234@gmail.com>
2026-09-22 12:12:00 +0700

    feat: menambahkan route group admin untuk tugas

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::resource('members', MemberController::class);
 Route::resource('loans', LoanController::class);
 Route::put('/loans/{id}/kembalikan', [LoanController::class, 'kembalikan'])
     ->name('loans.kembalikan');
+
+Route::prefix('admin')->group(function () {
+    
+    Route::get('/info', function () {
+        return 'Ini adalah halaman informasi Admin Perpustakaan';
+    });
+
+});
