49fb7b32b53dce587a1036e0199061923884eb5f
Akmal Marsya <akmalrasya234@gmail.com>
2026-09-22 11:33:14 +0700

    feat: menambahkan resource routes untuk books, categories, members, dan loans

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MemberController;
+use App\Http\Controllers\LoanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('books', BookController::class);
+Route::resource('members', MemberController::class);
+Route::resource('loans', LoanController::class);
+Route::resource('categories', CategoryController::class)->except(['show']);
